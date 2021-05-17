1da7273aa577c436df0ae9d4f9b36e7ab8415932
04010 <david.martinez340@comunidadunir.net>
2021-05-17 19:42:18 +0200

    corregidas pruebas unitarias

--- src/Calculadora/CalcTest.java
+++ src/Calculadora/CalcTest.java
@@ -5,21 +5,29 @@ package Calculadora;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 
-class CalcTest {
+public class CalcTest {
+	
+	double a;
+	double b;
+	Calculadora calc = null;
+
+	@Before
+	public void setUp() {
+		a = 25;
+		b = 5;
+		calc = new Calculadora();
+	}
 	
 	@Test
 	public void testSuma() {
 		// Arrange
-		double a = 25;
-		double b = 5;
-		Calculadora calc = null;
 		double resulSuma;
 		// Act
-		calc = new Calculadora();
 		resulSuma = calc.suma(a, b);
 		// Test
 		assertEquals(30, resulSuma);
@@ -28,12 +36,8 @@ class CalcTest {
 	@Test
 	public void testResta() {
 		// Arrange
-		double a = 25;
-		double b = 5;
-		Calculadora calc = null;
 		double resulResta;
 		// Act
-		calc = new Calculadora();
 		resulResta = calc.resta(a, b);
 		// Test
 		assertEquals(20, resulResta);
@@ -41,12 +45,8 @@ class CalcTest {
 	
 	public void testMultiplica() {
 		// Arrange
-		double a = 25;
-		double b = 5;
-		Calculadora calc = null;
 		double resulMultiplica;
 		// Act
-		calc = new Calculadora();
 		resulMultiplica = calc.multiplica(a, b);
 		// Test
 		assertEquals(125, resulMultiplica);
@@ -55,12 +55,8 @@ class CalcTest {
 	@Test
 	public void testDivide() {
 		// Arrange
-		double a = 25;
-		double b = 5;
-		Calculadora calc = null;
 		double resulDivide;
 		// Act
-		calc = new Calculadora();
 		resulDivide = calc.divide(a, b);
 		// Test
 		assertEquals(5, resulDivide);
@@ -69,11 +65,8 @@ class CalcTest {
 	@Test
 	public void testRaiz() {
 		// Arrange
-		double a = 25;
-		Calculadora calc = null;
 		double resulRaiz;
 		// Act
-		calc = new Calculadora();
 		resulRaiz = calc.raiz(a);
 		// Test
 		assertEquals(5, resulRaiz);
