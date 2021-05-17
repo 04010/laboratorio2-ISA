febae7efbfe43fcd2a7a84edbe94cd5c27ef77ef
04010 <david.martinez340@comunidadunir.net>
2021-05-17 19:24:49 +0200

    anadidas pruebas en clase test

--- src/Calculadora/CalcTest.java
+++ src/Calculadora/CalcTest.java
@@ -3,35 +3,39 @@
  */
 package Calculadora;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-/**
- * @author David
- *
- */
+
 class CalcTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void test() {
+		// Arrange
+		double a = 25;
+		double b = 5;
+		Calculadora calc = null;
+		double resulSuma;
+		double resulResta;
+		double resulMultiplica;
+		double resulDivide;
+		double resulRaiz;
+		// Act
+		calc = new Calculadora();
+		resulSuma = calc.suma(a, b);
+		resulResta = calc.resta(a, b);
+		resulMultiplica = calc.multiplica(a, b);
+		resulDivide = calc.divide(a, b);
+		resulRaiz = calc.raiz(a);
+		// Test
+		assertEquals(30, resulSuma);
+		assertEquals(20, resulResta);
+		assertEquals(125, resulMultiplica);
+		assertEquals(5, resulDivide);
+		assertEquals(5, resulRaiz);
 	}
 
 }
